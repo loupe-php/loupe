cdb495de67e9e0d966c00af177920f9a1cecf755
Yanick Witschi <yanick.witschi@terminal42.ch>
2023-08-03 11:41:50 +0200

    WIP

---
+++ install_lowest_supported_sqlite.sh
@@ -0,0 +1,22 @@
+cd ~
+
+# SQLite 3.7.17
+wget https://sqlite.org/2013/sqlite-autoconf-3071700.tar.gz
+
+tar zxvf sqlite-autoconf-3071700.tar.gz
+
+cd sqlite-autoconf-307170
+
+./configure --prefix=$HOME/opt/sqlite
+
+make
+make install
+
+export PATH=$HOME/opt/sqlite/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/opt/sqlite/lib
+export LD_RUN_PATH=$HOME/opt/sqlite/lib
+
+source .bash_profile
+
+which sqlite3
+sqlite3 --version
