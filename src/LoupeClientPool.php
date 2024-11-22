336049b0fb5db57b4e65110d047274857229857f
Philipp Daun <post@philippdaun.net>
2024-11-23 00:31:25 +0100

    Refactor
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/LoupeClientPool.php
+++ src/LoupeClientPool.php
@@ -1,10 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Loupe\Loupe;
 
-use Loupe\Loupe\Configuration;
-use Loupe\Loupe\Loupe;
-use Loupe\Loupe\LoupeFactory;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Path;
 
@@ -22,57 +21,32 @@ use Symfony\Component\Filesystem\Path;
 
 class LoupeClientPool
 {
-    protected LoupeFactory $factory;
-    protected Filesystem $filesystem;
-
     /**
      * @var Loupe[]
      */
     protected array $clients = [];
 
-    public function __construct(protected string $path)
-    {
+    protected LoupeFactory $factory;
+
+    protected Filesystem $filesystem;
+
+    public function __construct(
+        protected string $path
+    ) {
         $this->factory = new LoupeFactory();
         $this->filesystem = new Filesystem();
 
         // Create the base directory if it doesn't exist
-        if (! $this->filesystem->exists($this->path)) {
+        if (!$this->filesystem->exists($this->path)) {
             $this->filesystem->mkdir($this->path);
         }
-    }
-
-    public function get(string $index, Configuration $configuration): Loupe
-    {
-        return ($this->clients[$index] ??= $this->make($index, $configuration));
-    }
-
-    public function make(string $index, Configuration $configuration): Loupe
-    {
-        $this->createIndex($index);
-
-        return $this->factory->create($this->indexDirectory($index), $configuration);
-    }
-
-    public function indexDirectory(string $index): string
-    {
-        return Path::makeAbsolute($index, $this->path);
-    }
-
-    public function indexPath(string $index): string
-    {
-        return Path::makeAbsolute("{$index}/loupe.db", $this->path);
-    }
-
-    public function indexExists(string $index): bool
-    {
-        return $this->filesystem->exists($this->indexPath($index));
     }
 
     public function createIndex(string $index): void
     {
         $db = $this->indexPath($index);
-        if (! $this->filesystem->exists($db)) {
-            $this->filesystem->touch($db);
+        if (!$this->filesystem->exists($db)) {
+            $this->filesystem->dumpFile($db, '');
         }
     }
 
@@ -82,5 +56,32 @@ class LoupeClientPool
         if ($this->filesystem->exists($dir)) {
             $this->filesystem->remove($dir);
         }
+    }
+
+    public function get(string $index, Configuration $configuration): Loupe
+    {
+        return $this->clients[$index] ??= $this->make($index, $configuration);
+    }
+
+    public function indexDirectory(string $index): string
+    {
+        return Path::makeAbsolute($index, $this->path);
+    }
+
+    public function indexExists(string $index): bool
+    {
+        return $this->filesystem->exists($this->indexPath($index));
+    }
+
+    public function indexPath(string $index): string
+    {
+        return Path::makeAbsolute("{$index}/loupe.db", $this->path);
+    }
+
+    public function make(string $index, Configuration $configuration): Loupe
+    {
+        $this->createIndex($index);
+
+        return $this->factory->create($this->indexDirectory($index), $configuration);
     }
 }
