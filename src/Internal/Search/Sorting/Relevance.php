df2b94715d160e1468ea121e5e739e76ca6b88ea
Philipp Daun <post@philippdaun.net>
2024-11-23 20:19:07 +0100

    Calculate attribute weights per term
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/Internal/Search/Sorting/Relevance.php
+++ src/Internal/Search/Sorting/Relevance.php
@@ -28,7 +28,7 @@ class Relevance extends AbstractSorter
             // for the relevance split. Otherwise, the relevance calculation cannot know which of the documents did not match
             // because it's just a ";" separated list.
             $positionsPerDocument[] = sprintf(
-                "SELECT (SELECT COALESCE(group_concat(DISTINCT position), '0') FROM %s WHERE %s.id=document) AS %s",
+                "SELECT (SELECT COALESCE(group_concat(DISTINCT position || ':' || attribute), '0') FROM %s WHERE %s.id=document) AS %s",
                 $searcher->getCTENameForToken(Searcher::CTE_TERM_DOCUMENT_MATCHES_PREFIX, $token),
                 $engine->getIndexInfo()->getAliasForTable(IndexInfo::TABLE_NAME_DOCUMENTS),
                 Searcher::RELEVANCE_ALIAS . '_per_term',
@@ -39,11 +39,14 @@ class Relevance extends AbstractSorter
             return;
         }
 
+        $weights = $searcher->getSearchParameters()->getAttributeWeights();
+
         $select = sprintf(
-            "loupe_relevance((SELECT group_concat(%s, ';') FROM (%s)), %s) AS %s",
+            "loupe_relevance((SELECT group_concat(%s, ';') FROM (%s)), %s, '%s') AS %s",
             Searcher::RELEVANCE_ALIAS . '_per_term',
             implode(' UNION ALL ', $positionsPerDocument),
             $searcher->getTokens()->count(),
+            implode(';', array_map(fn($attr, $weight) => "{$attr}:{$weight}", array_keys($weights), $weights)),
             Searcher::RELEVANCE_ALIAS,
         );
 
@@ -71,13 +74,15 @@ class Relevance extends AbstractSorter
 
         foreach ($positionsPerTerm as $positions) {
             if ($positionPrev === null) {
-                $positionPrev = $positions[0];
+                [$position] = $positions[0];
+                $positionPrev = $position;
                 continue;
             }
 
             $distance = 0;
 
-            foreach ($positions as $position) {
+            foreach ($positions as $positionAndAttribute) {
+                [$position] = $positionAndAttribute;
                 if ($position > $positionPrev) {
                     $distance = $position - $positionPrev;
                     $positionPrev = $position;
@@ -98,34 +103,71 @@ class Relevance extends AbstractSorter
     }
 
     /**
-     * Example: A string with "3,8,10;0;4" would read as follows:
+     * @param array<int, array<int>> $positionsPerTerm
+     */
+    public static function calculateMatchCountFactor(array $positionsPerTerm, int $totalQueryTokenCount): float
+    {
+        $matchedTokens = array_filter($positionsPerTerm, function ($termArray) {
+            return !(\count($termArray) === 1 && $termArray[0][0] === 0);
+        });
+
+        return \count($matchedTokens) / $totalQueryTokenCount;
+    }
+
+    public static function calculateAttributeWeightFactor(array $positionsPerTerm, array $attributeWeights): float
+    {
+        $matchedAttributes = array_filter(array_map(fn ($term) => $term[0][1], $positionsPerTerm));
+        $matchedAttributeWeights = array_map(fn ($attribute) => $attributeWeights[$attribute] ?? 1, $matchedAttributes);
+
+        return array_sum($matchedAttributeWeights) / count($matchedAttributes);
+    }
+
+    /**
+     * Example: A string with "3:title,8:title,10:title;0;4:summary" would read as follows:
      * - The query consisted of 3 tokens (terms).
-     * - The first term matched. At positions 3, 8 and 10.
+     * - The first term matched. At positions 3, 8 and 10 in the `title` attribute.
      * - The second term did not match (position 0).
-     * - The third term matched. At position 4
+     * - The third term matched. At position 4 in the `summary` attribute.
      *
      * @param string $positionsInDocumentPerTerm A string of ";" separated per term and "," separated for all the term positions within a document
      */
-    public static function fromQuery(string $positionsInDocumentPerTerm, string $totalQueryTokenCount): float
+    public static function fromQuery(string $positionsInDocumentPerTerm, string $totalQueryTokenCount, string $attributeWeights): float
     {
+        ray($positionsInDocumentPerTerm, $totalQueryTokenCount, $attributeWeights);
+
         /**
-         * 1st: Number of query terms that match in a document.
-         * 2nd: Proximity of the words
+         * 1st: Number of query terms that match in a document
+         * 2nd: Weight of attributes matched
+         * 3rd: Proximity of the words
          */
-        static $relevanceWeights = [2, 1]; // Higher weight means more importance
-        static $totalWeight = 3; // Must be the sum of the above
+        static $relevanceWeights = [2, 1, 1]; // Higher weight means more importance
+        static $totalWeight = array_sum($relevanceWeights);
 
         $relevanceFactors = [];
         $totalQueryTokenCount = (int) $totalQueryTokenCount;
-        $positionsPerTerm = array_map(fn ($term) => array_map('intval', explode(',', $term)), explode(';', $positionsInDocumentPerTerm));
+        $positionsPerTerm = array_map(
+            fn ($term) => array_map(
+                fn ($position) => array_pad(explode(':', $position), 2, null),
+                explode(',', $term)
+            ),
+            explode(';', $positionsInDocumentPerTerm)
+        );
+        $attributeWeights = array_reduce(
+            explode(';', $attributeWeights),
+            function ($result, $item) {
+                [$key, $value] = explode(':', $item);
+                return [...$result, $key => (int) $value];
+            },
+            []
+        );
 
-        // 1st: Number of query terms that match in a document.
-        $totalMatchedTokens = \count(array_filter($positionsPerTerm, function ($termArray) {
-            return !(\count($termArray) === 1 && $termArray[0] === 0);
-        }));
-        $relevanceFactors[] = $totalMatchedTokens / $totalQueryTokenCount;
+        // 1st: Number of query terms that match in a document
+        $relevanceFactors[] =  self::calculateMatchCountFactor($positionsPerTerm, $totalQueryTokenCount);
 
-        // 2nd: Proximity
+        // 2nd: Weight of attributes matched
+        $relevanceFactors[] = self::calculateAttributeWeightFactor($positionsPerTerm, $attributeWeights);
+
+        // 3rd: Proximity of the words
         $relevanceFactors[] = self::calculateProximityFactor($positionsPerTerm);
 
         // Calculate weighted average
