5b1d67e5f8e44321c6f3e810d98c956b7deb7c3a
Philipp Daun <post@philippdaun.net>
2024-11-26 19:07:19 +0100

    Extend factory tests
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- tests/Unit/LoupeFactoryTest.php
+++ tests/Unit/LoupeFactoryTest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Loupe\Loupe\Tests\Unit;
 
+use Loupe\Loupe\Configuration;
+use Loupe\Loupe\Loupe;
 use Loupe\Loupe\LoupeFactory;
+use Loupe\Loupe\Tests\Util;
 use PHPUnit\Framework\TestCase;
 
 class LoupeFactoryTest extends TestCase
@@ -13,5 +16,19 @@ class LoupeFactoryTest extends TestCase
     {
         $factory = new LoupeFactory();
         $this->assertTrue($factory->isSupported());
+    }
+
+    public function testInMemoryClient(): void
+    {
+        $configuration = Configuration::create();
+        $client = (new LoupeFactory())->createInMemory($configuration);
+        $this->assertInstanceOf(Loupe::class, $client);
+    }
+
+    public function testPersistedClient(): void
+    {
+        $configuration = Configuration::create();
+        $client = (new LoupeFactory())->create(Util::fixturesPath('Storage/DB'), $configuration);
+        $this->assertInstanceOf(Loupe::class, $client);
     }
 }
