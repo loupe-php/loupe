61da876c2df1f1b6efd15ab00a8c0c43eb2c42ae
Yanick Witschi <yanick.witschi@terminal42.ch>
2024-11-29 10:57:54 +0100

    Update to toflar/state-set-index v3 (#118)

--- src/Internal/Search/Searcher.php
+++ src/Internal/Search/Searcher.php
@@ -501,7 +501,7 @@ class Searcher
             return implode(' ', $where);
         }
 
-        $states = $this->engine->getStateSetIndex()->findMatchingStates($term, $levenshteinDistance);
+        $states = $this->engine->getStateSetIndex()->findMatchingStates($term, $levenshteinDistance, 2);
 
         // No result possible, we add AND 1=0 to ensure no results
         if ($states === []) {
