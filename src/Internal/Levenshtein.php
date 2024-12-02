c94058d902905d9538bce9f7d4c64a9d266abf85
Yanick Witschi <yanick.witschi@terminal42.ch>
2024-12-02 15:52:47 +0100

    Switch to Damerau-Levenshtein

--- src/Internal/Levenshtein.php
+++ src/Internal/Levenshtein.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Loupe\Loupe\Internal;
 
+use Toflar\StateSetIndex\DamerauLevenshtein;
+
 class Levenshtein
 {
-    public static function levenshtein(string $string1, string $string2, bool $firstCharTypoCountsDouble): int
+    public static function damerauLevenshtein(string $string1, string $string2, bool $firstCharTypoCountsDouble): int
     {
-        $distance = \Toflar\StateSetIndex\Levenshtein::distance($string1, $string2);
+        $distance = DamerauLevenshtein::distance($string1, $string2);
 
         if ($firstCharTypoCountsDouble && mb_substr($string1, 0, 1) !== mb_substr($string2, 0, 1)) {
             $distance++;
@@ -17,13 +19,14 @@ class Levenshtein
         return $distance;
     }
 
-    public static function maxLevenshtein(string $string1, string $string2, int $max, bool $firstCharTypoCountsDouble): bool
+    public static function maxLevenshtein(string $string1, string $string2, int $maxDistance, bool $firstCharTypoCountsDouble): bool
     {
-        // Maybe this can be optimized in the future. We're looking if $string1's distance to $string2 is
-        // more or equal to $max. This means we do not care about the actual distance, just the boolean result.
-        // So we don't have to e.g. count to a distance of 10 if $max is 2, we can early return.
-        // Not sure if worth it, though as implementing it in PHP would be slower than in C so it might end up having
-        // the same performance.
-        return self::levenshtein($string1, $string2, $firstCharTypoCountsDouble) <= $max;
+        $distance = DamerauLevenshtein::distance($string1, $string2, $maxDistance + 1);
+
+        if ($firstCharTypoCountsDouble && mb_substr($string1, 0, 1) !== mb_substr($string2, 0, 1)) {
+            ++$distance;
+        }
+
+        return $distance <= $maxDistance;
     }
 }
