1a28ffb5ac990e8652bb633781d2039cd92acb88
Philipp Daun <post@philippdaun.net>
2024-12-05 19:58:34 +0100

    Simplify memoization
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/Traits/MemoizationTrait.php
+++ src/Traits/MemoizationTrait.php
@@ -4,22 +4,19 @@ namespace Loupe\Loupe\Traits;
 
 trait MemoizationTrait
 {
-    /**
-     * @var array<string, mixed>
-     */
-    private array $memo = [];
-
     private function memoize(callable $compute): callable
     {
-        $namespace = serialize($compute);
-
-        return function () use ($namespace, $compute) {
+        return function () use ($compute) {
+            /**
+             * @var array<string, mixed>
+             */
+            static $cache = [];
             $args = \func_get_args();
-            $key = $namespace . ':' . implode(':', $args);
-            if (!\array_key_exists($key, $this->memo)) {
-                $this->memo[$key] = \call_user_func_array($compute, $args);
+            $key = implode('--', $args);
+            if (!\array_key_exists($key, $cache)) {
+                $cache[$key] = \call_user_func_array($compute, $args);
             }
-            return $this->memo[$key];
+            return $cache[$key];
         };
     }
 }
