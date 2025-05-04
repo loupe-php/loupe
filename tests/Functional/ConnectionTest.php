5578a06d3c8ad2f007c016ed773f730239cd92ed
Philipp Daun <post@philippdaun.net>
2025-05-04 17:52:41 +0200

    Test optimizations are only run once
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- tests/Functional/ConnectionTest.php
+++ tests/Functional/ConnectionTest.php
@@ -7,6 +7,7 @@ namespace Loupe\Loupe\Tests\Functional;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Tools\DsnParser;
 use Loupe\Loupe\Configuration;
+use Loupe\Loupe\Logger\InMemoryLogger;
 use Loupe\Loupe\LoupeFactory;
 use Loupe\Loupe\Tests\StorageFixturesTestTrait;
 use PHPUnit\Framework\TestCase;
@@ -26,6 +27,27 @@ class ConnectionTest extends TestCase
         $this->assertSame('wal', $this->createConnection($dir)->fetchOne('PRAGMA journal_mode'));
     }
 
+    public function testOptimizationsOnlyRunOnce(): void
+    {
+        $dir = $this->createTemporaryDirectory();
+        $logger = new InMemoryLogger();
+        $configuration = Configuration::create()->withLogger($logger);
+
+        $loupe = (new LoupeFactory())->create($dir, $configuration);
+        $loupe->addDocument([
+            'id' => 1,
+            'title' => 'Test',
+        ]);
+
+        $loupe = (new LoupeFactory())->create($dir, $configuration);
+        $loupe->addDocument([
+            'id' => 2,
+            'title' => 'Test',
+        ]);
+
+        $this->assertCount(1, $this->getLoggedQueries($logger, 'PRAGMA page_size'));
+    }
+
     public function testPageSize(): void
     {
         $dir = $this->createTemporaryDirectory();
@@ -40,5 +62,20 @@ class ConnectionTest extends TestCase
     private function createConnection(string $dir): \Doctrine\DBAL\Connection
     {
         return DriverManager::getConnection((new DsnParser())->parse('sqlite3://notused:inthis@case/' . $dir . '/loupe.db'));
+    }
+
+    /**
+     * @return array<string>
+     */
+    private function getLoggedQueries(InMemoryLogger $logger, ?string $filter = null): array
+    {
+        $records = array_filter($logger->getRecords(), fn (array $record) => str_contains((string) $record['message'], 'Executing query'));
+        $queries = array_map(fn (array $record) => $record['context']['sql'], $records);
+
+        if ($filter) {
+            return array_filter($queries, fn (string $query) => str_contains($query, $filter));
+        }
+
+        return $queries;
     }
 }
