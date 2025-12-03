fea3b077f6d3949f02682eb948f4bca9debbb372
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-12-03 10:38:10 +0100

    Move determining the term length to the preparation step rather than the indexing step (#254)

--- src/Internal/Index/PreparedDocument/Term.php
+++ src/Internal/Index/PreparedDocument/Term.php
@@ -6,12 +6,15 @@ namespace Loupe\Loupe\Internal\Index\PreparedDocument;
 
 class Term
 {
+    private int $termLength;
+
     public function __construct(
         private string $term,
         private string $attribute,
         private int $position,
         private bool $isVariant
     ) {
+        $this->termLength = mb_strlen($term, 'UTF-8');
     }
 
     public function getAttribute(): string
@@ -27,6 +30,11 @@ class Term
     public function getTerm(): string
     {
         return $this->term;
+    }
+
+    public function getTermLength(): int
+    {
+        return $this->termLength;
     }
 
     public function isVariant(): bool
